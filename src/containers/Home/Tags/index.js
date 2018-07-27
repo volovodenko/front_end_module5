bc14986f39e7cdb6e4eacf365728986ee417f33b
Vitaly Volovodenko <volovodenko@gmail.com>
2018-07-27 11:49:30 +0300

    patch 12

--- src/containers/Home/Tags/index.js
+++ src/containers/Home/Tags/index.js
@@ -58,7 +58,7 @@ export default class Tags extends Component {
             this.maxTagRows = Math.ceil(this.props.galleryTagsList.length / this.props.numberTagsInRow);
         }
 
-        console.log('this.maxTagRows='this.maxTagRows);
+        console.log('this.maxTagRows='+this.maxTagRows);
 
         if (this.props.sortTagsBy === 'posts') {
             this.sortByPosts();
